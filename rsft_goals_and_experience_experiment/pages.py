a11023a1d9caf7c2f21eb36e38fbf194fe08fa9a
Jana Jarecki <jarecki.jana@gmail.com>
2022-05-16 23:20:56 +0200

    initial

---
+++ rsft_goals_and_experience_experiment/pages.py
@@ -0,0 +1,187 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+# class Prolificid(Page):
+#   form_model = 'player'
+#   form_fields = ['prolificid', 'browser']
+#   def is_displayed(self):
+#     return self.round_number == 1
+
+# ENGLISCH -----------------------------------------------------------------------
+
+class Coverstory_learning_eng(Page):
+  form_model = 'player'
+  def get_form_fields(player):
+    if 'sample_condition' == 0:
+      return ["c1e"]
+    else:
+      return ["c1e", "c4e", "c6e"]
+  def is_displayed(self):
+    return self.player.phase in ["familiarization"]
+  def vars_for_template(self):
+    return {
+      'num_samples': Constants.num_samples,
+      'num_trials': Constants.num_trials,
+      'goal_condition': self.participant.vars['goal_condition'],
+      'sample_condition': self.participant.vars['sample_condition']
+    }
+
+class Coverstory_choice_eng(Page):
+  form_model = 'player'
+  form_fields = ["c2e", "c3e", "c5e"]
+  def is_displayed(self):
+    return self.player.phase in ["familiarization"]
+  def vars_for_template(self):
+    return {
+      'num_samples': Constants.num_samples,
+      'num_trials': Constants.num_trials,
+      'goal_condition': self.participant.vars['goal_condition'],
+      'sample_condition': self.participant.vars['sample_condition']
+    }
+
+class Coverstory_sum_eng(Page):
+  def is_displayed(self):
+    return self.round_number == (self.player.phase in ["familiarization"])
+  form_model = 'player'
+  form_fields = ["c7e"]
+  def vars_for_template(self):
+    return {
+      'num_samples': Constants.num_samples,
+      'num_trials': Constants.num_trials,
+      'sample_condition': self.participant.vars['sample_condition']
+    }
+
+class Instruction_Choices_eng(Page):
+  def is_displayed(self):
+    return self.round_number == (self.player.phase in ["familiarization"])
+  def vars_for_template(self):
+    return {
+      'num_trials': Constants.num_trials,
+      'sample_condition': self.participant.vars['sample_condition'],
+      'num_samples': Constants.num_samples,
+    }
+
+class Coverstory_check_eng(Page):
+  def vars_for_template(self):
+    return {
+      'goal_condition': self.participant.vars['goal_condition'],
+      'sample_condition': self.participant.vars['sample_condition'],
+    }
+  form_model = 'player'
+  def get_form_fields(player):
+    if 'sample_condition' == 0:
+        return ["g1e","g2e", "g3e", "g4e", "g5e"]
+    else:
+        return ["l1e", "l2e", "l3e", "l4e"]
+  def is_displayed(self):
+    return self.player.phase in ["familiarization"]
+
+class Incentives_eng(Page):
+  form_model = 'player'
+  form_fields = ["i4e"]
+  def is_displayed(self):
+    return self.player.phase in ["familiarization"]
+  def vars_for_template(self):
+    return({
+    'participation_fee': self.participant.payoff_plus_participation_fee(),
+    'bonus_amount': c(1).to_real_world_currency(self.session),
+    'num_trials': Constants.num_trials,
+    'sample_condition': self.participant.vars['sample_condition'],
+    #'bonus_amount': Constants.bonus_amount,
+    })
+
+class NewBlock_eng(Page):
+  def is_displayed(self):
+    return (self.round_number > 1)
+  form_model = 'player'
+  def vars_for_template(self):
+    context =  self.player.vars_for_template()
+    p = self.player
+    context.update({
+      'currentblock': p.block,
+      'budget': p.budget,
+      'success': p.get_last_success(),
+      'successes': p.update_successes(),
+      "pstart": p.state,
+      'round_number': self.round_number - 1,
+      })
+    return context
+
+class Sample(Page):
+  def is_displayed(self):
+    return (self.player.phase in ["familiarization", "stimuli_easy", "attentioncheck_easy", "stimuli_medium", "attentioncheck_medium", "stimuli_hard", "attentioncheck_hard"])
+  form_model = 'player'
+  def get_form_fields(self):
+    samplefields = ['sample{}'.format(i) for i in range(1, Constants.num_samples + 1)]
+    drawfields = ['draw{}'.format(i) for i in range(1, Constants.num_samples + 1)]
+    samplertfields = ['sample_rt_ms{}'.format(i) for i in range(1, Constants.num_samples + 1)]
+    return samplefields + drawfields + samplertfields
+  def vars_for_template(self):
+    context = self.player.vars_for_template()
+    context.update({
+      'outcomes': self.participant.vars['sampling_outcomes'][self.round_number],
+      'successes': self.player.get_last_success(),
+      'goal_condition': self.participant.vars['goal_condition'],
+      'sample_condition': self.participant.vars['sample_condition'],
+      'round_number': self.round_number - 1,
+    })
+    return context
+
+
+class Choices(Page):
+  def is_displayed(self):
+    return (self.player.phase in ["familiarization", "stimuli_easy", "attentioncheck_easy", "stimuli_medium", "attentioncheck_medium", "stimuli_hard", "attentioncheck_hard"])
+  form_model = 'player'
+  def get_form_fields(self):
+    choicefields = ['choice{}'.format(i) for i in range(1, Constants.num_trials + 1)]
+    statefields = ['state{}'.format(i) for i in range(1, Constants.num_trials + 2)]
+    rtfields = ['rt_ms{}'.format(i) for i in range(1, Constants.num_trials + 1)]
+    return choicefields + statefields + rtfields + ['success'] + ['successes']
+  def vars_for_template(self):
+    context =  self.player.vars_for_template()
+    context.update({
+      'outcomes': self.participant.vars['outcomes'][self.round_number],
+      'successes': self.player.get_last_success(),
+      'num_rounds': Constants.num_rounds,
+      'num_trials': Constants.num_trials,
+      'round_number': self.round_number - 1,
+    })
+    return context
+  def before_next_page(self):
+    self.player.draw_bonus()
+
+
+class Payment(Page):
+  def is_displayed(self):
+    return self.round_number >= Constants.num_rounds
+  def vars_for_template(self):
+    return {
+      'participation_fee': c(self.session.config['participation_fee']).to_real_world_currency(self.session),
+      # 'bonus': c(self.player.bonus).to_real_world_currency(self.session),
+      'bonus': c(self.player.draw_bonus()).to_real_world_currency(self.session)
+      }
+
+class Attentionchecks_explanation(Page):
+  def is_displayed(self):
+    return self.player.phase in ["familiarization"]
+  form_model = 'player'
+  form_fields = ["a1e"]
+  def vars_for_template(self):
+    return {
+    'duration': Constants.duration,
+    }
+
+page_sequence = [
+  Attentionchecks_explanation,
+  Coverstory_learning_eng,
+  NewBlock_eng,
+  Sample,
+  Coverstory_choice_eng,
+  Choices,
+  Coverstory_check_eng,
+  Incentives_eng,
+  Coverstory_sum_eng,
+  Instruction_Choices_eng,
+]
---
+++ settings.py
@@ -0,0 +1,133 @@
+from os import environ
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+mturk_hit_settings = dict({
+    'keywords': ['risk', 'goals', 'academic', 'experience', 'study'],
+    'template': 'global/mturk_template.html',
+    'title': 'Goals and Experience ($5.025 for about 40 min, $2 possible bonus)',
+    'description': 'Decide between options to reach a goal',
+    'frame_height': 800,
+    'minutes_allotted_per_assignement': 80,
+    'expiration_hours': 7 * 24,
+    'qualification_requirements': [
+        # Only US
+        {
+            'QualificationTypeId': "00000000000000000071",
+            'Comparator': "EqualTo",
+            'LocaleValues': [{'Country': "US"}]
+        },
+        # At least 200 HITs approved
+        {
+            'QualificationTypeId': "00000000000000000040",
+            'Comparator': "GreaterThanOrEqualTo",
+            'IntegerValues': [200]
+        },
+        # At least 95% of HITs approved
+        {
+            'QualificationTypeId': "000000000000000000L0",
+            'Comparator': "GreaterThanOrEqualTo",
+            'IntegerValues': [95]
+        },
+        ]
+}),
+
+
+USE_I18N = True
+
+MIDDLEWARE = [
+  'django.middleware.security.SecurityMiddleware',
+  'whitenoise.middleware.WhiteNoiseMiddleware'
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SESSION_CONFIG_DEFAULTS = {
+    'real_world_currency_per_point': 0.25,
+    'participation_fee': 0.00,
+    'doc': '',
+    'mturk_hit_settings': mturk_hit_settings
+}
+
+
+SESSION_CONFIGS = [
+    dict(name='rsft_goals_and_experience_experiment',
+        display_name="RSFT goals and experience",
+        num_demo_participants=4,
+        participation_fee=5.025,
+        app_sequence=[
+            'consent_page',
+            'mpl',
+            'rsft_goals_and_experience_experiment',
+            'survey'
+            ],
+        real_world_currency_per_point = 0.25,
+        use_browser_bots = False,
+        ),
+]
+
+
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en-us'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ROOMS = [
+    dict(
+        name='econ101',
+        display_name='Econ 101 class',
+        participant_label_file='_rooms/econ101.txt',
+    ),
+    dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
+]
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
+
+DEMO_PAGE_INTRO_HTML = """
+Here are some oTree games.
+"""
+
+# don't share this with anybody.
+SECRET_KEY = 'd$vavnmwdqxbhtn@5ue^6dbhvez+)ue4mz+6$&!1#^n!=+g7e$'
+
+INSTALLED_APPS = ['otree']
+
+# inactive session configs
+# dict(name='trust', display_name="Trust Game", num_demo_participants=2, app_sequence=['trust', 'payment_info']),
+# dict(name='prisoner', display_name="Prisoner's Dilemma", num_demo_participants=2,
+#      app_sequence=['prisoner', 'payment_info']),
+# dict(name='volunteer_dilemma', display_name="Volunteer's Dilemma", num_demo_participants=3,
+#      app_sequence=['volunteer_dilemma', 'payment_info']),
+# dict(name='cournot', display_name="Cournot Competition", num_demo_participants=2, app_sequence=[
+#     'cournot', 'payment_info'
+# ]),
+# dict(name='dictator', display_name="Dictator Game", num_demo_participants=2,
+#      app_sequence=['dictator', 'payment_info']),
+# dict(name='matching_pennies', display_name="Matching Pennies", num_demo_participants=2, app_sequence=[
+#     'matching_pennies',
+# ]),
+# dict(name='traveler_dilemma', display_name="Traveler's Dilemma", num_demo_participants=2,
+#      app_sequence=['traveler_dilemma', 'payment_info']),
+# dict(name='bargaining', display_name="Bargaining Game", num_demo_participants=2,
+#      app_sequence=['bargaining', 'payment_info']),
+# dict(name='common_value_auction', display_name="Common Value Auction", num_demo_participants=3,
+#      app_sequence=['common_value_auction', 'payment_info']),
+# dict(name='bertrand', display_name="Bertrand Competition", num_demo_participants=2, app_sequence=[
+#     'bertrand', 'payment_info'
+# ]),
+# dict(name='public_goods_simple', display_name="Public Goods (simple version from tutorial)",
+#      num_demo_participants=3, app_sequence=['public_goods_simple', 'payment_info']),
+# dict(name='trust_simple', display_name="Trust Game (simple version from tutorial)", num_demo_participants=2,
+#      app_sequence=['trust_simple']),
